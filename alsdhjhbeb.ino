8be9bf6c41f0c1c6b279f53d69cbee1e04c0f369
Albert Dillon <albert.dillon@uqconnect.edu.au>
2018-09-18 16:57:33 +1000

    Filters Written
    Filters written for Pan-Tompkin method.  To-do: Thresholds

--- alsdhjhbeb.ino
+++ alsdhjhbeb.ino
@@ -7,9 +7,12 @@
 
 #define FILTER_LENGTH           100
 #define STORED_DATA_LENGTH      100
+#define INTEGRATION_WINDOW      10
 
 #define SAMPLE_FREQUENCY        200
-#define SAMPLE_PERIOD           (1000000 / SAMPLE_FREQUENCY)
+#define SAMPLE_PERIOD           (1 / SAMPLE_FREQUENCY)
+#define SAMPLE_PERIOD_MS        (1000 / SAMPLE_FREQUENCY)
+#define SAMPLE_PERIOD_US        (1000000 / SAMPLE_FREQUENCY)
 
 #define CONSOLE_LOG             1
 
@@ -20,9 +23,14 @@ typedef enum {
 } Flag;
 
 typedef struct {
-    int filter[FILTER_LENGTH];
-    int storedInput[STORED_DATA_LENGTH];
-    int storedOutput[STORED_DATA_LENGTH];
+    int storedIn[STORED_DATA_LENGTH];
+    int LPOut[STORED_DATA_LENGTH];
+    int HPOut[STORED_DATA_LENGTH];
+    int derOut[STORED_DATA_LENGTH];
+    int squOut[STORED_DATA_LENGTH];
+    int MWOut[STORED_DATA_LENGTH];
+    int storedOut[STORED_DATA_LENGTH];
+    int index;
     int in;
     int out;
     Flag QRSFlag;
@@ -36,8 +44,12 @@ PanTompkin Data;
 int CurrentTime, LastTime;
 
 /************************ Private Function Prototypes *************************/
-int apply_filter(int dataIn);
-void create_filter(void);
+void apply_filter(PanTompkin* data);
+void low_pass(int* x, int* y, int length, int i);
+void high_pass(int* x, int* y, int length, int i);
+void derivative(int* x, int* y, int length, int i);
+void squaring(int* x, int* y, int length, int i);
+void moving_window(int* x, int* y, int length, int i);
 
 /**************************** Function Definitions ****************************/
 
@@ -65,8 +77,8 @@ void setup(void) {
 void loop(void) {
     CurrentTime = micros();
 
-    if ((CurrentTime - LastTime > SAMPLE_PERIOD) ||
-            (LastTime - CurrentTime > SAMPLE_PERIOD)) {
+    if ((CurrentTime - LastTime > SAMPLE_PERIOD_US) ||
+            (LastTime - CurrentTime > SAMPLE_PERIOD_US)) {
         // Absolute difference > Sample period
         LastTime = micros();
 
@@ -96,37 +108,77 @@ void loop(void) {
     Takes a data point as an integer.  Applies digital filters.  Returns next 
     output.
 */
-int apply_filter(PanTompkin* data) {
-  
-    return data->out;
+void apply_filters(PanTompkin* data) {
+    low_pass(data->storedIn, data->LPOut, STORED_DATA_LENGTH, data->index);
+    high_pass(data->LPOut, data->HPOut, STORED_DATA_LENGTH, data->index);
+    derivative(data->HPOut, data->derOut, STORED_DATA_LENGTH, data->index);
+    squaring(data->derOut, data->squOut, STORED_DATA_LENGTH, data->index);
+    moving_window(data->squOut, data->MWOut, STORED_DATA_LENGTH, data->index);
+
+    return;
 }
 
-/*
-    Populates filter.
-*/
-void create_filter(int* filter, unsigned length) {
-    int T = SAMPLE_PERIOD;
-
+void low_pass(int* x, int* y, int length, int i) {
     // Low-Pass Filter
+    int a = (i - 1)  % length;
+    int b = (i - 2)  % length;
+    int c = (i - 6)  % length;
+    int d = (i - 12) % length;
+    
     // y(nT) = 2 * y(nT - T) - y(nT-2T) + x(nT) - 2 * x(nT-6T) + x(nT - 12T);
+    y[i] = 2 * y[a] - y[b] + x[i] - 2 * x[c] + x[d];
 
+    return;
+}
+
+void high_pass(int* x, int* y, int length, int i) {
     // High-Pass Filter
+    int a = (i - 16) % STORED_DATA_LENGTH;
+    int b = (i - 1)  % STORED_DATA_LENGTH;
+    int c = (i - 32) % STORED_DATA_LENGTH;
+
     // y(nT) = 32 * x(nT - 16T) - (y(nT - T) + x(nT) - x(nT - 32T));
+    y[i] = 32 * x[a] - (y[b] + x[i] - x[c]);
 
-    // Derivative Filter
+    return;
+}
+
+void derivative(int* x, int* y, int length, int i) {
+    // Derivative filter
+
+    int a = (i - 4) % STORED_DATA_LENGTH;
+    int b = (i - 3) % STORED_DATA_LENGTH;
+    int c = (i - 1) % STORED_DATA_LENGTH;
+
     // y(nT) = (1/8T)*(-x(nT - 2T) - 2x(nT - T) + 2x(nT + T) + x(nT + 2T));
+    y[i] = SAMPLE_FREQUENCY * (- x[a] - 2 * x[b] + 2 * x[c] + x[i]) / 8;
 
+    return;
+}
+
+void squaring(int* x, int* y, int length, int i) {
     // Squaring Function
+
     // y(nT) = x(nT) * x(nT);
+    y[i] = x[i] * x[i];
 
-    int N = 10; // Integration window size
+    return;
+}
+
+void moving_window(int* x, int* y, int length, int i) {
     // Moving-Window Integration
+
     // y(nT) = (x[nT - (N-1)*T] + x[nT - (N-2)*T] + ... + x(nT))/N;
+    y[i] = 0;
+    for (int a = 0; a < INTEGRATION_WINDOW; a++) {
+        y[i] += x[(i - a) % STORED_DATA_LENGTH)];
+    }
+    y[i] /= INTEGRATION_WINDOW;
 
-    // Fiducial Mark
+    return;
+}
 
-    // Adjusting Thresholds
-
+void peak_i_threshold(void) {
     /*
     peakI // Overall peak
 
@@ -147,7 +199,10 @@ void create_filter(int* filter, unsigned length) {
     // When found on searchback:
     SPKI = (peakI + 3 * SPKI) / 4;
     */
+    return;
+}
 
+void peak_f_threshold(void) {
     /*
 
     peakF // Overall peak
@@ -167,28 +222,50 @@ void create_filter(int* filter, unsigned length) {
     // When found on searchback:
     SPKF = (peakF + 3 * SPKF) / 4;
     */
+    return;
+}
 
+void rr_interval(void) {
     // Adjusting Average RR Interval and Rate limits
     // RRAve = (Rn + ... + R[n-7]) / 8
 
     // 
     // RRAveTune = (Rn' + ... + R[n-7]) / 8;
-    // Where Rn' are 
+    // tuneUpper = (Rn * 116) / 100;
+    // tuneLower = (Rn * 92) / 100;
+    // tuneMissed = (Rn * 166) / 100;
+    // if complex not found during interval specified by tuneMissed,
+    //  -> Take maximal peak between established thresholds is QRS candidate.
+    // if each of 8 RR intervals from RRAve within specified interval,
+    // -> Heart Rate regular. RRAveTune = RRAve. 
+    
 
+    // Twave identification
+    // RR interval <360ms >200ms, justify whether QRS or t-wave.
+    // If maximal slope during waveform < previous QRS / 2, T-wave, otherwise
+    // QRS.
+    return;
+}
 
-    for (int i = 0; i < length; i++) {
-        filter[i] = 0;
-    }
+void detect_peaks(PanTompkin* data) {
+
+    peak_i_threshold();
+    peak_f_threshold();
+    rr_interval();    
+
     return;
 }
 
 /*
-    Pan-Tompkin method.
+    Populates filter.
 */
-void detect_qrs(PanTompkin* data) {
-    if (data->in == 0) {
-        digitalWrite(LED_QRS_PIN, HIGH);
-        data->QRSFlag = RAISED;
+void create_filter(int* filter, unsigned length) {
+
+    
+
+
+    for (int i = 0; i < length; i++) {
+        filter[i] = 0;
     }
     return;
 }
